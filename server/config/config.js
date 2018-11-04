fab9776a63af05f09a70bfe0d2be4eab654220b1
Tom O <9ooooooooo@protonmail.com>
2018-11-04 23:23:57 +1000

    removed mongodb access string

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ if (env === 'development'){
 		let keys = require('./keys.js');
 		process.env.WEATHER_TOKEN = keys.weatherToken;
 		process.env.GEOCODE_TOKEN = keys.geocodeToken;
-		process.env.MONGODB_URI = 'mongodb://test1:testtest1@ds251223.mlab.com:51223/todoapp';
+		process.env.MONGODB_URI = keys.mongoDbURI;
 		process.env.PORT = 3000;
 		process.env.JWT_SECRET = "wrdjahdfiuewhuhy3763ehfh376r67!@#$#%%&ggtftfre$#@#%fgtrffd$#$$%";
 		
