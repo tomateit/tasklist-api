3d991cff9a070e031ca0ff6108a2cd75a6d384f3
Tom O <fatclient@protonmail.com>
2019-09-15 02:49:27 +1000

    extended test

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,9 +7,12 @@ import { IUserModel } from "../src/server/models/user";
 import { UserSchema } from "../src/server/schemas/user";
 
 dotenv.config();
+
 const connection: mongoose.Connection = mongoose.createConnection(
   String(process.env.TEST_MONGODB_URI),
   {
+    useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
@@ -19,6 +22,10 @@ const User: mongoose.Model<IUserModel> = connection.model<IUserModel>(
   "User",
   UserSchema
 );
+
+// beforeAll(async () => {
+//   return ;
+// })
 
 describe("My work", () => {
   test("works", () => {
@@ -32,19 +39,44 @@ expect(myBool).not.toBe(false);
 //   .toBeTruthy()
 
 describe("User", () => {
+  const email = "adfu2@hfjh.dk";
+  const password = "pwdpwdpwd";
+  const username = "USERNAME_TEST";
+
   it("Should create a new User", async () => {
     const user: IUser = {
-      email: "adfu@hfjh.dk",
-      username: "USERTEST",
-      password: "pwdpwdpwd"
+      email,
+      password,
+      username
     };
 
     const result = await new User(user).save();
 
     expect(result).toHaveProperty("_id");
-    expect(result.email).toMatch("adfu@hfjh.dk");
-    expect(result.username).toMatch("USERTEST");
+    expect(result).toHaveProperty("email", email);
+    expect(result).toHaveProperty("username", username);
     expect(result).toHaveProperty("password");
-    expect(result.password).not.toMatch("pwdpwdpwd");
+    expect(result).toHaveProperty("createdAt");
+    expect(result.password).not.toMatch(password);
   });
+
+  const updatedEmail = "tested2@updatedmail.com";
+
+  it("Should update the User", async () => {
+    const RetrievedUser = await User.findOneAndUpdate(
+      { email },
+      { $set: { email: updatedEmail } },
+      { new: true }
+    );
+    expect(RetrievedUser).toHaveProperty("_id");
+    expect(RetrievedUser).toHaveProperty("email", updatedEmail);
+  });
+
+  afterAll(async () => {
+    await User.deleteMany({});
+  });
+});
+
+afterAll(async () => {
+  await connection.close();
 });
