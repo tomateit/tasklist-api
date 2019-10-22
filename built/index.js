604b5a4a4123226fe6a96b3910a8c68f30f99498
Tom O <fatclient@protonmail.com>
2019-10-23 03:01:53 +1000

    human readable port indication

--- built/index.js
+++ built/index.js
@@ -4,9 +4,8 @@ const dontenv = require("dotenv");
 const app_1 = require("./server/app");
 dontenv.config();
 const port = process.env.PORT;
-console.log(port);
 const s = new app_1.default();
 s.app.listen(port, () => {
-    console.log("listening...");
+    console.log(`listening at port ${port}...`);
 });
 //# sourceMappingURL=index.js.map
