aa1d726061d05d752f7d20c5064c82c61977775d
Tom O <maxmax@localhost.localdomain>
2019-11-26 10:07:11 +0000

    bot can be activated

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,4 +1,4 @@
-import Telegraf from 'telegraf';
+import Telegraf  from 'telegraf';
 
 
 export default class TelegramBot {
@@ -7,19 +7,37 @@ export default class TelegramBot {
 
 
     constructor(token: string|undefined) {
+        if (!token) {
+            throw new Error("NO TOKEN PROVIDED")
+        }
         this.telegraf = new Telegraf(token);
     }
 
     public launch(cb: ()=> void) {
         this.demo();
+        this.start_script();
+        this.fallback_script();
+
         this.telegraf.launch()
         cb()
     }
 
+    private start_script() {
+        const bot = this.telegraf;
+        bot.start((ctx:any) => ctx.reply("Hello!"))
+    }
+
     private demo() {
         const bot = this.telegraf;
-        bot.command('oldschool', (ctx: any) => ctx.reply('Hello'))
+        bot.command('meta', (ctx: any) => ctx.reply(`from: ${ctx.from} \n chat: ${ctx.chat} \n updateType: ${ctx.updateType} \n updateSubTypes: ${ctx.updateSubTypes}`))
         // bot.command('modern', ({ reply: }) => reply('Yo'))
         bot.command('hipster', Telegraf.reply('λ'))
+    }
+
+    private fallback_script() {
+        const bot = this.telegraf;
+        bot.catch((err: any, ctx: any) => {
+            console.error(`ERR_TGBOT on ${ctx.updateType}`, err)
+        })
     }
 }
