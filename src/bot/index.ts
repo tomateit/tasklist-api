04e204ff0094c11d8efe46a892c3c5c21f9f1150
Tom O <maxmax@localhost.localdomain>
2019-11-26 15:35:05 +0000

    trying new functionality

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,5 +1,7 @@
 import Telegraf  from 'telegraf';
-
+import { session } from "telegraf";
+import help from "./scenes/help";
+import menu from "./scenes/menu";
 
 export default class TelegramBot {
 
@@ -8,7 +10,7 @@ export default class TelegramBot {
 
     constructor(token: string|undefined) {
         if (!token) {
-            throw new Error("NO TOKEN PROVIDED")
+            throw new Error("NO TOKEN PROVIDED");
         }
         this.telegraf = new Telegraf(token);
     }
@@ -16,28 +18,36 @@ export default class TelegramBot {
     public launch(cb: ()=> void) {
         this.demo();
         this.start_script();
+        this.menu_script();
         this.fallback_script();
 
-        this.telegraf.launch()
-        cb()
+        this.telegraf.launch();
+        cb();
     }
 
     private start_script() {
         const bot = this.telegraf;
-        bot.start((ctx:any) => ctx.reply("Hello!"))
+        bot.start((ctx:any) => ctx.reply("Bot started. "));
+        help(bot);
+    }
+
+    private menu_script() {
+        const bot = this.telegraf;
+
+        menu(bot);
     }
 
     private demo() {
         const bot = this.telegraf;
-        bot.command('meta', (ctx: any) => ctx.reply(`from: ${ctx.from} \n chat: ${ctx.chat} \n updateType: ${ctx.updateType} \n updateSubTypes: ${ctx.updateSubTypes}`))
+        bot.command('meta', (ctx: any) => ctx.reply(`from: ${JSON.stringify(ctx.from, null, 2)} \n chat: ${JSON.stringify(ctx.chat, null, 2)} \n updateType: ${ctx.updateType} \n updateSubTypes: ${ctx.updateSubTypes}`));
         // bot.command('modern', ({ reply: }) => reply('Yo'))
-        bot.command('hipster', Telegraf.reply('λ'))
+        bot.command('hipster', Telegraf.reply('λ'));
     }
 
     private fallback_script() {
         const bot = this.telegraf;
         bot.catch((err: any, ctx: any) => {
-            console.error(`ERR_TGBOT on ${ctx.updateType}`, err)
+            console.error(`ERR_TGBOT on ${ctx.updateType}`, err);
         })
     }
 }
